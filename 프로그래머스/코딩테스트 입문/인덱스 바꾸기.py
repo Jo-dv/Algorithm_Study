e01bef68651bb367773e789eb42f1f0a9bd012bc
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2023-03-04 20:24:17 +0900

    230304

---
+++ 프로그래머스/코딩테스트 입문/인덱스 바꾸기.py
@@ -0,0 +1,8 @@
+my_string, num1, num2 = 'hello', 1, 2
+
+def solution(my_string, num1, num2):
+    my_string = list(my_string)
+    my_string[num1], my_string[num2] = my_string[num2], my_string[num1]
+    return ''.join(my_string)
+
+print(solution(my_string, num1, num2))

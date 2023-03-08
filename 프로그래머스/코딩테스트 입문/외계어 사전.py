35c7d9d520e6b65a856dafa534098526e44705fc
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2023-03-08 20:12:08 +0900

    230308

--- 프로그래머스/코딩테스트 입문/외계어 사전.py
+++ 프로그래머스/코딩테스트 입문/외계어 사전.py
@@ -5,6 +5,6 @@ def solution(spell, dic):
     for i in dic:  # 각 단어마다
         for j in spell:  # 스펠링의 수를 카운팅
             temp[i].append(i.count(j))
-    return 1 if [1] * len(spell) in temp.values() else 2  # 카운팅 된 값이 스펠링에 대해 각각 1씩 있다면
+    return 1 if [1] * len(spell) in temp.values() else 2  # 카운팅된 값이 스펠링에 대해 각각 1씩 있다면
 
 print(solution(spell, dic))

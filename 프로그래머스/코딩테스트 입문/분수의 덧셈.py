cbafd9c5d045091b7e16fa00cdf142bc702a43a1
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2023-03-13 08:20:35 +0900

    230223

--- 프로그래머스/코딩테스트 입문/분수의 덧셈.py
+++ 프로그래머스/코딩테스트 입문/분수의 덧셈.py
@@ -8,6 +8,6 @@ def solution(numer1, denom1, numer2, denom2):
     n, m = numer1 * denom2 + numer2 * denom1, denom1*denom2  # 분수계산식
     b = gcd(n, m)  # 최대공약수를 구하여
     return [n // b, m // b]  # 기약분수 계산
-    # 덧붙여, b * (n // b) * (m // b) = 최소공배수
+    # 덧붙여, (n * m)//b = 최소공배수
 
 print(solution(numer1, denom1, numer2, denom2))

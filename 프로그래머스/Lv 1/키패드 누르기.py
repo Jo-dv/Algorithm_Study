2395ef3a495aa10ae3b4fbef405a1980e86fe3c9
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2023-03-24 16:38:56 +0900

    optimize

--- 프로그래머스/Lv 1/키패드 누르기.py
+++ 프로그래머스/Lv 1/키패드 누르기.py
@@ -1,14 +1,15 @@
 def solution(numbers, hand):
     answer = ''
-    keypad = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ['*', 0, '#']]  # 4 x 3 매트릭스
+    keypad = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ['*', 0, '#']]  # 가상 키패드
+    col = list(zip(*keypad))
     pos = {k: (y, x) for y, lines in enumerate(keypad) for x, k in enumerate(lines)}  # 각 키에 대한 위치 저장
     l_hand, r_hand = '*', '#'  # 처음 시작 위치
 
     for target in numbers:  # 눌러야 할 번호가
-        if target in list(zip(*keypad))[0]:  # 누를 수 있는 곳이 왼쪽에 위치해 있다면
+        if target in col[0]:  # 누를 수 있는 곳이 왼쪽에 위치해 있다면
             l_hand = target  # 왼손은 해당 번호로 이동
             answer += 'L'  # 그리고 왼손을 사용했으므로 답에 왼손 추가
-        elif target in list(zip(*keypad))[2]:  # 누를 수 있는 곳이 오른쪽에 위치해 있다면
+        elif target in col[2]:  # 누를 수 있는 곳이 오른쪽에 위치해 있다면
             r_hand = target  # 오른손을 해당 번호로 이동
             answer += 'R'  # 그리고 오른손을 사용했으므로 답에 오른손 추가
         else:  # 가운데에 위치해 있다면
@@ -29,4 +30,6 @@ def solution(numbers, hand):
 targets = [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]
 hand = "left"
 
-print(solution(targets, hand))
+#print(solution(targets, hand))
+a, b = [1, 2, 3]
+print(b)

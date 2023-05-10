455986242c9e42066d758a994eaf02ae13529c6e
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2023-05-10 09:36:00 +0900

    230509

--- swea/D3/3282 01 Knapsack.py
+++ swea/D3/3282 01 Knapsack.py
@@ -26,10 +26,13 @@ for t in range(1, int(input()) + 1):
     for ni in range(1, n + 1):  # 각 아이템에 대해
         for ki in range(1, k + 1):  # 제한이 1 ~ K 까지일 때 최적의 값을 저장
             if items[ni - 1][v] > ki:  # 현재 선택된 아이템의 부피가 Ki의 부피를 가진 가방보다 크다면
-                answer[ni][ki] = answer[ni - 1][ki]  # 직전에 값을 그대로 저장
+                answer[ni][ki] = answer[ni - 1][ki]  # 현재 무게에 해당하는 직전에 값을 그대로 저장
             else:  # 만약 그렇지 않고 담을 수 있다면
-                answer[ni][ki] = max(answer[ni - 1][ki], answer[ni - 1][ki - items[ni - 1][v]] + items[ni - 1][c])
-                # answer[ni - 1][ki]: ni번째 아이템을 선택하지 않은 경우
-                # ki - items[ni - 1][v]: 현재 무게에서 선택된 아이템의 무게를 뺐을 때 들어있는 가치
-                # items[ni - 1][c]: 선택한 아이템의 가치
+                previous = answer[ni - 1][ki]  # 현재 무게에 해당하는 직전에 값
+                select = items[ni - 1]  # 현재 선택된 아이템
+                temp = ki - select[v]  # 현재 무게 - 선택된 아이템의 무게 = 인덱스 = 남은 무게에 해당하는 아이템의 무게 인덱스
+                update = answer[ni - 1][temp] + select[c]  # 이전 아이템과 현재 아이템 합산
+                answer[ni][ki] = max(previous, update)  # 이전 아이템의 가치와 합산된 아이템의 가치 중 큰 것 선택
+                # 가령, 3번째 아이템(부피:2, 가치:3)을 선택하고 현재 부피(인덱스)가 4라면 이전 아이템의 부피 2에 해당하는 아이템의 가치에
+                # 선택된 아이템의 가치(3)을 더한다. 그 후 해당 아이템을 선택하지 않았을 때의 가치와 선택된 아이템의 가치를 비교해서 갱신
     print(f'#{t} {answer[-1][-1]}')

0164008f8cbdb0024d17639cee0d43e64b1c4199
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2023-05-21 00:33:08 +0900

    230520

--- 백준/실버/2667 단지번호붙이기.py
+++ 백준/실버/2667 단지번호붙이기.py
@@ -1,3 +1,4 @@
+'''
 def dfs(y, x):
     global count
 
@@ -24,6 +25,42 @@ for i in range(n):
             town += 1  # 단지 수 갱신
             counts.append(count)  # 단지의 가구 수 저장
             count = 0  # 처리가 끝나면 가구 수 초기화
+
+print(town)
+for i in sorted(counts):
+    print(i)'''
+
+from collections import deque
+
+def bfs(y, x):
+    q = deque([(y, x)])
+    d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    count = 1  # 1일 때 함수가 실행되므로 1부터 계산함
+
+    maps[y][x] = 0  # 시작 부분 탐색 완료 처리
+
+    while q:
+        y, x = q.popleft()
+        for dy, dx in d:
+            my, mx = y + dy, x + dx
+            if 0 <= my < n and 0 <= mx < n and maps[my][mx] == 1:  # 범위를 벗어나지 않고 탐색하지 않았다면
+                maps[my][mx] = 0  # 이동한 곳 탐색 완료 처리
+                q.append((my, mx))  # 이동한 좌표 저장
+                count += 1  # 가구 수 갱신
+
+    return count
+
+
+n = int(input())
+maps = [list(map(int, input())) for _ in range(n)]
+town = 0
+counts = []
+
+for i in range(n):
+    for j in range(n):
+        if maps[i][j]:
+            counts.append(bfs(i, j))
+            town += 1
 
 print(town)
 for i in sorted(counts):

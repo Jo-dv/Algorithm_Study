05b62ea3afa0d8a3af6a7c049e31f8efca6928c7
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2023-07-13 08:47:07 +0900

    Update 파리퇴치3.py
    230712

--- swea/D2/파리퇴치3.py
+++ swea/D2/파리퇴치3.py
@@ -3,13 +3,13 @@ for t in range(1, int(input())+1):
     grid = [list(map(int, input().split())) for _ in range(n)]
     answer = 0
 
-    plus = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    plus = [[-1, 0], [1, 0], [0, -1], [0, 1]]  # 스프레이 방향 벡터
     cross = [[-1, -1], [1, -1], [-1, 1], [1, 1]]
 
     for y in range(n):  # 탐색의 방향은 왼쪽에서 오른쪽, 위에서 아래로 진행
         for x in range(n):
             plus_result, cross_result = grid[y][x], grid[y][x]  # 스프레이 형태: 십자, 엑스자 시작 값
-            for plus_d, cross_d in zip(plus, cross):  # 스프레이 방향 벡터
+            for plus_d, cross_d in zip(plus, cross):  # 스프레이 방향 벡터 값을 하나씩 가져와서
                 for power in range(1, m):  # 시작 값은 위에서부터 받았기 때문에 1 ~ m-1
                     plus_y, plus_x = y + plus_d[0] * power, x + plus_d[1] * power  # 해당 위치에 스프레이의 세기를 1씩 증가
                     cross_y, cross_x = y + cross_d[0] * power, x + cross_d[1] * power

c2cc88b11fe101c3ba45f55aed313c1d9cd5572d
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2023-07-15 13:12:07 +0900

    230713

--- swea/D2/파리퇴치3.py
+++ swea/D2/파리퇴치3.py
@@ -2,8 +2,6 @@ for t in range(1, int(input())+1):
     n, m = map(int, input().split())
     grid = [list(map(int, input().split())) for _ in range(n)]
     answer = 0
-
-<<<<<<< HEAD
     plus = [[-1, 0], [1, 0], [0, -1], [0, 1]]
     cross = [[-1, -1], [1, -1], [-1, 1], [1, 1]]
 
@@ -21,22 +19,3 @@ for t in range(1, int(input())+1):
                 answer = max(answer, plus_result, cross_result)
 
     print(f'#{t} {answer}')
-=======
-    plus = [[-1, 0], [1, 0], [0, -1], [0, 1]]  # 스프레이 방향 벡터
-    cross = [[-1, -1], [1, -1], [-1, 1], [1, 1]]
-
-    for y in range(n):  # 탐색의 방향은 왼쪽에서 오른쪽, 위에서 아래로 진행
-        for x in range(n):
-            plus_result, cross_result = grid[y][x], grid[y][x]  # 스프레이 형태: 십자, 엑스자 시작 값
-            for plus_d, cross_d in zip(plus, cross):  # 스프레이 방향 벡터 값을 하나씩 가져와서
-                for power in range(1, m):  # 시작 값은 위에서부터 받았기 때문에 1 ~ m-1
-                    plus_y, plus_x = y + plus_d[0] * power, x + plus_d[1] * power  # 해당 위치에 스프레이의 세기를 1씩 증가
-                    cross_y, cross_x = y + cross_d[0] * power, x + cross_d[1] * power
-                    if -1 < plus_y < n and -1 < plus_x < n:  # 증가된 스프레이의 값이 범위를 벗어나지 않은 경우
-                        plus_result += grid[plus_y][plus_x]  # 해당 위치에 파리 값 합산
-                    if -1 < cross_y < n and -1 < cross_x < n:
-                        cross_result += grid[cross_y][cross_x]
-                answer = max(answer, plus_result, cross_result)  # 반환할 값을 가장 큰 값으로 갱신
-
-    print(f'#{t} {answer}')
->>>>>>> 8f7a847317a2056438602ebb56d63da139824bcd

0cdab50f9d94a95318c97d11707cdc01324d90b9
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2023-09-28 16:29:54 +0900

    20230928

--- Python/백준/실버/11723 집합.py
+++ Python/백준/실버/11723 집합.py
@@ -1,49 +1,27 @@
 import sys
 
-m = int(input())
-ansewr = []
-## 울 재 ~~~
-class sol:
+class main:
     def __init__(self):
-        self.s = list()
+        self.m = int(input())
+        self.s = set()
 
-    def add(self, x):
-        if x not in self.s:
-            self.s.append(x)
+    def solve(self):
+        for _ in range(self.m):
+            i = sys.stdin.readline().rstrip().split()  # 명령 및 출력을 따로 받아서 처리하면 메모리 혹은 시간 초과 발생
+            if i[0] == "add":
+                self.s.add(int(i[1]))
+            elif i[0] == "remove":
+                self.s.discard(int(i[1]))
+            elif i[0] == "check":
+                print(1 if int(i[1]) in self.s else 0)
+            elif i[0] == "toggle":
+                if int(i[1]) in self.s:
+                    self.s.remove(int(i[1]))
+                else:
+                    self.s.add(int(i[1]))
+            elif i[0] == "all":
+                self.s = set((range(1, 21)))
+            else:
+                self.s.clear()
 
-    def remove(self, x):
-        if x in self.s:
-            self.s.remove(x)
-
-    def check(self, x):
-        ansewr.append('1' if x in self.s else '0')
-
-    def toggle(self, x):
-        self.s.remove(x) if x in self.s else self.s.append(x)
-
-    def all(self):
-        self.s = [str(i) for i in range(1, 21)]
-
-    def empty(self):
-        self.s = []
-
-problem = sol()
-
-for _ in range(m):
-    op = sys.stdin.readline().split()
-
-    if op[0] == 'add' and op[1] not in problem.s:
-        problem.add(op[1])
-    elif op[0] == 'remove' and op[1] in problem.s:
-        problem.remove(op[1])
-    elif op[0] == 'check':
-        problem.check(op[1])
-    elif op[0] == 'toggle':
-        problem.toggle(op[1])
-    elif op[0] == 'all':
-        problem.all()
-    else:
-        problem.empty()
-
-for i in ansewr:
-    print(i)
+main = main().solve()

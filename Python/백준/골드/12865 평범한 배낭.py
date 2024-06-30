876b21a8d4bfccd5063518599193f00e1c633789
Jo-dv <whgjsgh11@gmail.com>
2024-07-01 00:11:07 +0900

    20240630

--- Python/백준/골드/12865 평범한 배낭.py
+++ Python/백준/골드/12865 평범한 배낭.py
@@ -8,6 +8,9 @@ class Main:
         for w, v in self.item:  # 아이템 순서대로
             for i in range(self.k, w - 1, -1):  # 최대 무게에서 현재 아이템 무게까지
                 self.bag[i] = max(self.bag[i], self.bag[i - w] + v)  # 현재 무게의 가치 비선택 or 선택
+                # self.bag[i - w] + v
+                # 현재 아이템 w의 무게를 포함한 가치를 합산하려면, 현재 무게에서 w를 뺀 무게를 확인해야 한다.
+                # 다시 말하면, 현재 무게가 7이고 담으려는 무게가 3이라면 7-3 -> 4일 때의 무게와 합산해서 비교한다는 의미
 
         print(self.bag[self.k])
 

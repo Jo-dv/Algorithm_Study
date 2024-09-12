082530baff8f479b3e309009093f3d4fdd328638
Jo-dv <whgjsgh11@gmail.com>
2024-09-12 18:40:36 +0900

    20240912

---
+++ Java/src/백준/실버/BOJ_11656.java
@@ -0,0 +1,27 @@
+package 백준.실버;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.TreeSet;
+
+public class BOJ_11656 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static String s;
+	static TreeSet<String> suffix = new TreeSet<>();
+	
+	public static void main(String[] args) throws IOException {
+		s = br.readLine();
+		
+		solve();
+	}
+	
+	static void solve() {
+		int len = s.length();
+		for(int i = 0; i < len; i++)
+			suffix.add(s.substring(i));
+		
+		for(String i: suffix)
+			System.out.println(i);
+	}
+}

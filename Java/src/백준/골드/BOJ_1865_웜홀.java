9d48993e3b864ec82a0cf99c4594865f40b79f03
Jo-dv <whgjsgh11@gmail.com>
2024-09-16 00:10:07 +0900

    20240915

--- Java/src/백준/골드/BOJ_1865_웜홀.java
+++ Java/src/백준/골드/BOJ_1865_웜홀.java
@@ -66,11 +66,12 @@ public class BOJ_1865_웜홀 {
 		for(int i = 1; i <= n; i++) {
 			boolean update = false;
 			for(Edge current: edges) {
-				if(cost[current.start] != Integer.MAX_VALUE && cost[current.end] > cost[current.start] + current.time) {
+				if(cost[current.start] != Integer.MAX_VALUE && cost[current.end] > cost[current.start] + current.time) {  // 현재 도달할 수 있고 최소로 갱신가능하다면
 					cost[current.end] = cost[current.start] + current.time;
 					update = true;
 					if(i == n)
 						return true;
+					// 최단 경로 특성상 갱신을 진행하다보면 특정 값에 수렴하는데, 음수 가중치가 있을경우 무한히 수렴하므로 모든 정점에 대해 검사했을 때도 갱신이 일어나면 사이클이 발생하는 것
 				}
 			}
 			
@@ -85,7 +86,7 @@ public class BOJ_1865_웜홀 {
 		boolean flag = false;
 		
 		for(int i = 1; i <= n; i++)
-			if(bf(i)) {
+			if(bf(i)) {  // 사이클이 발생하는 정점 탐색
 				flag = true;
 				break;
 			}
